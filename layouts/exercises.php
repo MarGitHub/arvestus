ef5519e0f83eb8046f410c897dcbd5c9175057b8
Margit <margit.pilden@gmail.com>
2014-09-30 13:12:11 +0300

    ÜL 4.1, 4.2

--- layouts/exercises.php
+++ layouts/exercises.php
@@ -12,9 +12,13 @@
       <section id="site-content">
         <h2>Tingimuslause</h2>
         <!--  Sisesta siia kood -->
-
+		<?php $nr = rand(100 , 200); ?>
+		<?php 
+		if (150<$nr) { echo "Juhuslik number {$nr} on suurem kui 150";} 
+		elseif	(150>$nr) { echo "Juhuslik number {$nr} on väiksem kui 150";} ;?>
         <h2>Juhuslik pilt</h2>
         <!--  Sisesta siia kood -->
+		<img src="http://users.khk.ee/kaspar.naaber/svp/periood-1/arvestus/images/<?php echo rand(1,13);?>.jpg"/>
 
         <h2>Kõikide piltide väljastamine</h2>
         <!--  Sisesta siia kood -->
--- layouts/news.php
+++ layouts/news.php
@@ -2,6 +2,7 @@
 <html>
 <head>
   <?php include "../components/site-data.php"; ?>
+  <?php include "../components/html-head.php"; ?>
 </head>
 
 <body>
@@ -12,14 +13,14 @@
       <section id="site-content">
               <article class="blog-post"> 
 			   <?php
-+          $i = 0;
-+            foreach ($blog_news as $news){
-+                if(++$i > 2) break;
-+                echo '<article><h2 class="post-title">'.$news["post_title"].'</h2>';
-+                echo '<div class="post-excerpt">'.$news["post_excerpt"].'</div>';
-+                echo '<div class="post-body">'.$news["post_body"].'</div></article>';
-+            };
-+          ?>
+          $i = 0;
+           foreach ($blog_news as $news){
+              if(++$i > 2) break;
+              echo '<article><h2 class="post-title">'.$news["post_title"].'</h2>';
+              echo '<div class="post-excerpt">'.$news["post_excerpt"].'</div>';
+               echo '<div class="post-body">'.$news["post_body"].'</div></article>';
+          };
+         ?>
 		  </article>
       </section>
     </main>
